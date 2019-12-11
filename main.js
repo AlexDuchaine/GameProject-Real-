60cb69d39aa8ccfffc802c7b8b5e6f3016907766
AlexDuchaine <55748399+AlexDuchaine@users.noreply.github.com>
2019-12-11 15:16:43 -0500

    Added Animation for Main
    Player now jumps, visuals work

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ let keys = {};
 canvas.width = 1000;
 canvas.height = 600;
 
+
+    let player = new Player();
+    
+    function animate() {
+        requestAnimationFrame(animate);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+            //Bottom platform
+            context.fillStyle = "#663300";
+            context.strokeStyle = "#0d0d0d";
+            context.fillRect(0, 500, 1000, 10);
+            context.strokeRect(0, 500, 1000, 10);
+
+            if(keys.w && player.isJumping == false) {
+                player.jump(); //2D Motion
+            }
+
+        
+            player.update();
+            
+        }
+
+    
+
+    animate();
+    canvas.focus();
